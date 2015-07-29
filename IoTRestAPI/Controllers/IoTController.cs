471d38f58f988515b62cb589b2403a2ba0ec34f9
Bill Baker <billb@magenic.com>
2015-07-29 12:34:04 -0500

    Added a Web API only project

---
+++ IoTRestAPI/Controllers/IoTController.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace IoTRestAPI.Controllers
+{
+    public class IoTController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post(HttpRequestMessage requestMessage)
+        {
+            var json = string.Empty;
+            JArray events = null;
+
+            try
+            {
+                json = await requestMessage.Content.ReadAsStringAsync();
+                dynamic request = JObject.Parse(json);
+                events = (JArray)request.events;
+
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch (Exception)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                throw;
+            }
+        }
+    }
+}
---
+++ IoTRestAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IoTRestAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IoTRestAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("641b7ad2-1c20-424e-bc26-5ea40b1fb6e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
